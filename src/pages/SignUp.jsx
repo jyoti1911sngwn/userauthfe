3a174d5e96642be052127ec78dba1b55797d328b
jyoti1911sngwn <145776387+jyoti1911sngwn@users.noreply.github.com>
2024-12-25 07:09:01 -0800

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,7 +10,7 @@ const SignUp = () => {
     }
     const handlesubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+        const response = await fetch("https://express-backend-ngby.onrender.com/api/auth/createuser", {
             method: "POST", 
             headers: {
                 'Content-Type': 'application/json'
